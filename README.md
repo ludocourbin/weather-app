0505a88d9722fa1b8411c99a54d5dc81012a2aea
Ludovic <ludovic.courbin@gmail.com>
2020-05-09 22:11:37 +0200

    v2 weather app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # weather-app
+
+App that will tell you the weather in the city you ask, you can see the temperature, sunrise time, sunset time
--- script.js
+++ script.js
@@ -7,8 +7,6 @@
 // console.log(moment);
 const API = '2636c5cb22df3006dd1caad933db8a81';
 
-let city = 'paris';
-
 const app = {
 	domElts: {
 		userChoice: document.getElementById('user-choice'),
@@ -20,22 +18,25 @@ const app = {
 		wind: document.getElementById('wind'),
 		sunrise: document.getElementById('sunrise'),
 		sunset: document.getElementById('sunset'),
+		textToSpeech: document.getElementById('text'),
 	},
 	userCityChoice: 'paris',
 };
 
 console.log(app.domElts.icon);
+let myUrl;
 
 app.domElts.userChoice.addEventListener('keyup', function (e) {
 	if (e.key === 'Enter') {
 		app.userCityChoice = app.domElts.userChoice.value;
+		myUrl = `https://source.unsplash.com/1600x900/?${app.userCityChoice}`;
+		console.log(myUrl);
+		document.body.style.background = `url("${myUrl}")`;
 		userAction();
 	}
 });
 
 //openweathermap.org/weather-conditions
-
-// https://community.algolia.com/places/
 
 const userAction = async () => {
 	const response = await fetch(
@@ -43,7 +44,7 @@ const userAction = async () => {
 	);
 	const myJson = await response.json(); //extract JSON from the http response
 	// do something with myJson
-	console.log(myJson);
+	// console.log(myJson);
 	app.domElts.city.textContent = myJson.name;
 	app.domElts.country.textContent = myJson.sys.country;
 	app.domElts.description.textContent = myJson.weather[0].description;
@@ -51,6 +52,16 @@ const userAction = async () => {
 		'src',
 		'http://openweathermap.org/img/wn/' + myJson.weather[0].icon + '@2x.png'
 	);
+
+	if (myJson.weather[0].description.includes('rain')) {
+		app.domElts.icon.setAttribute('src', './images/rain.svg');
+	} else if (myJson.weather[0].description.includes('clouds')) {
+		app.domElts.icon.setAttribute('src', './images/sun.png');
+	} else if (myJson.weather[0].description.includes('clear')) {
+		app.domElts.icon.setAttribute('src', './images/sun.svg');
+	} else if (myJson.weather[0].description.includes('sun')) {
+		app.domElts.icon.setAttribute('src', './images/sun.svg');
+	}
 
 	app.domElts.temperature.textContent = myJson.main.temp;
 	app.domElts.sunrise.textContent = formatDate(
@@ -61,12 +72,56 @@ const userAction = async () => {
 		myJson.sys.sunset,
 		myJson.timezone
 	);
+
+	msg.text = `In ${myJson.name} it is ${myJson.main.temp} degrees it is at the moment ${app.domElts.description.textContent} and the wind is blowing at ${app.domElts.wind.textContent} kilometer an hour, further information about this city, the sunset it's at ${app.domElts.sunset.textContent} and tomorrow, the sun will be rising at ${app.domElts.sunrise.textContent}. The weather app wish you a wonderful day`;
+
+	console.log(msg.text);
 };
 
 userAction();
 
 function formatDate(timestamp, utc) {
 	let date = new Date((timestamp + utc - 7200) * 1000);
-	let datevalues = date.getHours() + 'h ' + date.getMinutes() + 'min ';
+	let datevalues = date.getHours() + 'h' + date.getMinutes() + 'min ';
 	return datevalues;
 }
+
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+
+const voicesDropdown = document.querySelector('[name="voice"]');
+// const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector('#speak');
+const stopButton = document.querySelector('#stop');
+
+function populateVoices() {
+	voices = this.getVoices();
+	const voicesOptions = voices
+		.map(
+			(voice) =>
+				`<option value ="${voice.name}">${voice.name}(${voice.lang})</option>`
+		)
+		.join('');
+	voicesDropdown.innerHTML = voicesOptions;
+}
+
+function setVoice() {
+	msg.rate = 0;
+	console.log('changing voice');
+	msg.voice = voices.find((voice) => voice.name === this.value);
+	toggle();
+}
+
+function toggle(startOver = true) {
+	speechSynthesis.cancel();
+	if (startOver) {
+		speechSynthesis.speak(msg);
+	}
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+voicesDropdown.addEventListener('change', setVoice);
+speakButton.addEventListener('click', toggle);
+stopButton.addEventListener('click', function () {
+	toggle(false);
+});
